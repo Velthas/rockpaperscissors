1c9df7ec1c00b4758badf3c047e055842c9a24a2
Velthas <damianobattaglia97@gmail.com>
2021-12-22 11:15:15 +0100

    Adjusted js for ui, added style for score box

--- script.js
+++ script.js
@@ -1,48 +1,56 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const lizard = document.querySelector(".lizard");
+const spock = document.querySelector(".spock");
+
 function computerPlay() {
-    let generateNumber = (Math.floor(Math.random() * 3)) + 1;
+    let generateNumber = (Math.floor(Math.random() * 5)) + 1;
     switch (generateNumber) {
         case 1:
-            return "Rock";
+            return "ROCK";
 
         case 2:
-            return "Paper";
+            return "PAPER";
 
         case 3: 
-            return "Scissors";   
+            return "SCISSORS"; 
+        
+        case 4: 
+            return "LIZARD";
+        
+        case 5:
+            return "SPOCK";
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    let capPlayerChoice = playerSelection.toUpperCase();
-    let capCompChoice = computerSelection.toUpperCase();
-    
-    //Check if the user is being naughty
-    if  (capPlayerChoice !== "ROCK" && capPlayerChoice !== "PAPER" && capPlayerChoice !== "SCISSORS") {
-        return "Write rock, paper or scissors";
-    }
+rock.addEventListener('click', () => playRound("ROCK", computerPlay()));
+paper.addEventListener('click', () => playRound("PAPER", computerPlay()));
+scissors.addEventListener('click', () => playRound("SCISSORS", computerPlay()));
+spock.addEventListener('click', () => playRound("SPOCK", computerPlay()));
+lizard.addEventListener('click', () => playRound("LIZARD", computerPlay()));
 
+const scoreDiv = document.querySelector('#score');
+const results = document.createElement('div');
+
+function playRound(playerSelection, computerSelection) {
+    console.log(computerSelection)
+    scoreDiv.appendChild(results);
+    results.classList.add("comment");
+    
     //Check for a winner or a loser based on comparing the two selections.
-    if  (capPlayerChoice === "ROCK" && computerSelection === "Scissors" || capPlayerChoice === "PAPER" && computerSelection === "Rock" ||  capPlayerChoice === "SCISSORS" && computerSelection === "Paper") {
-        return "Player 1 is the winner!";
+    if  (playerSelection === "ROCK" && computerSelection === "SCISSORS" || playerSelection === "PAPER" && computerSelection === "ROCK" ||  playerSelection === "SCISSORS" && computerSelection === "PAPER" || playerSelection === "ROCK" && computerSelection === "LIZARD" || playerSelection === "LIZARD" && computerSelection === "SPOCK" || playerSelection === "SPOCK" &&computerSelection === "SCISSORS" || playerSelection === "PAPER" && computerSelection === "SPOCK" || playerSelection === "SCISSORS" && computerSelection === "LIZARD" || playerSelection === "SPOCK" && computerSelection === "ROCK" || playerSelection === "LIZARD" && computerSelection === "PAPER") {
+        results.textContent = `One point\n for humanity!\nThe computer really didn't see ${playerSelection} coming!`
     }
     //If the computer's choice and the player's are the same, then it's a tie
-    else if (capPlayerChoice === capCompChoice) {
+    else if (playerSelection === computerSelection) {
 
-        return "It's a tie for now";
+        results.textContent = "It's a tie! Get back at it!"
     }
     //In all other cases, we lost
     else {
-        return "The machines have won.";
+        results.textContent = `The machines have won this round, keep going!` 
     }
 
 }
 
-//This function simulates a game with five rounds in it.
-function playGame () {
-    //Loop through five times
-    for (let i = 0; i < 5 ; i++) {
-        //Ask for the player's move each time, then generate the computers and declare the winner.
-        console.log(playRound(prompt("Give me your move"), computerPlay()));
-    }
-
-}
